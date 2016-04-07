76d25d28931db95032a285c610e58a83b69f4fa8
jason <jason-white@live.cn>
2016-04-07 13:40:44 +0800

    下棋落子

--- app/src/main/java/com/wyb/gobang/Gobang.java
+++ app/src/main/java/com/wyb/gobang/Gobang.java
@@ -5,8 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //五子棋自定义View
 public class Gobang extends View {
@@ -18,7 +23,11 @@ public class Gobang extends View {
     private Paint paint = new Paint();
 
     private Bitmap whitePiece, blackPiece;
-    private float ratioPieceOfLineHeight = 3 * 1.0f / 4 ;
+    private float ratioPieceOfLineHeight = 3 * 1.0f / 4;
+
+    private boolean isWhite = true;
+    private List<Point> whiteArray = new ArrayList<>();
+    private List<Point> blackArray = new ArrayList<>();
 
     public Gobang(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -70,18 +79,57 @@ public class Gobang extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBoard(canvas);
+        drawPiece(canvas);
     }
 
     //绘制棋盘
     private void drawBoard(Canvas canvas) {
         int w = mPanelWidth;
         float LineHeight = mLineHeight;
-        for (int i=0; i<MAX_LINE; i++) {
+        for (int i = 0; i < MAX_LINE; i++) {
             int startX = (int) (LineHeight / 2);
             int endX = (int) (w - LineHeight / 2);
             int y = (int) ((0.5 + i) * LineHeight);
             canvas.drawLine(startX, y, endX, y, paint);
             canvas.drawLine(y, startX, y, endX, paint);
         }
+    }
+
+    //绘制棋子
+    private void drawPiece(Canvas canvas) {
+        for (int i = 0, n = whiteArray.size(); i < n; i++) {
+            Point whitePoint = whiteArray.get(i);
+            canvas.drawBitmap(whitePiece, (whitePoint.x + (1 - ratioPieceOfLineHeight) / 2) * mLineHeight, (whitePoint.y + (1 - ratioPieceOfLineHeight) / 2) * mLineHeight, null);
+        }
+        for (int i = 0, n = blackArray.size(); i < n; i++) {
+            Point blackPoint = blackArray.get(i);
+            canvas.drawBitmap(blackPiece, (blackPoint.x + (1 - ratioPieceOfLineHeight) / 2) * mLineHeight, (blackPoint.y + (1 - ratioPieceOfLineHeight) / 2) * mLineHeight, null);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            Point pointPiece = getValidPoint(x, y);
+            if (whiteArray.contains(pointPiece) || blackArray.contains(pointPiece)) {
+                return false;
+            }
+            if (isWhite) {
+                whiteArray.add(pointPiece);
+            } else {
+                blackArray.add(pointPiece);
+            }
+            invalidate();  //重新绘制
+            isWhite = !isWhite;
+            return true;
+        }
+        return true;
+    }
+
+    //计算用户点击的棋盘点（保证点击的地方只能下一个棋子）
+    private Point getValidPoint(int x, int y) {
+        return new Point((int) (x / mLineHeight), (int) (y / mLineHeight));
     }
 }
